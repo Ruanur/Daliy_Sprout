73b8fa2a9aa40a5b45d1f81bc8387ad244f8ab1c
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-03-26 06:25:56 +0900

    [PGS] 택배 상자 꺼내기/Lv.1/3시간
    https://school.programmers.co.kr/learn/courses/30/lessons/388351

---
+++ alsrudwls/Programmers Lv.1 유연근무제.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+static int Work(vector<int>& schedules, vector<vector<int>>& timelogs, queue<int> day) {
+    int result = 0;
+
+    for (int i = 0; i < schedules.size(); i++) {
+        queue<int> days = day;
+        bool isEligible = true;
+        for (int j = 0; j < 7; j++) {
+            int current_day = days.front();
+            days.pop();
+
+            if (current_day == 6 || current_day == 7) {
+                continue;
+            }
+
+            if (timelogs[i][j] > schedules[i] + 10) {
+                isEligible = false;
+                break;
+            }
+        }
+
+        if (isEligible) {
+            result++;
+        }
+    }
+    return result;
+}
+
+int solution(vector<int> schedules, vector<vector<int>> timelogs, int startday) {
+    int answer = 0;
+    queue<int> day;
+
+    for (int i = 0; i < 7; i++) {
+        day.push((startday + i - 1) % 7 + 1);
+    }
+
+    answer = Work(schedules, timelogs, day);
+
+    return answer;
+}
