2893d1fa097774fc9ea1f0af37c1526923724c44
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-05-01 12:06:46 +0900

    [PGS] 마법의 엘리베이터/Lv.2/2시간
    https://school.programmers.co.kr/learn/courses/30/lessons/148653

---
+++ alsrudwls/Programmers Lv.2 마법의 엘리베이터.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int calc(int storey) {
+    int answer = 0;
+
+    while (storey > 0) {
+        int last_digit = storey % 10;
+        int next_digit = (storey / 10) % 10; // 다음 자리 확인
+
+        if (last_digit > 5 || (last_digit == 5 && next_digit >= 5)) {
+            storey += (10 - last_digit); // 10의 배수로 올림
+        }
+        else {
+            storey -= last_digit; // 10의 배수로 내림
+        }
+
+        answer += min(last_digit, 10 - last_digit); // 최소 클릭 횟수 추가
+        storey /= 10; // 다음 자리로 이동
+    }
+
+    return answer;
+}
+
+int solution(int storey) {
+    int answer = 0;
+
+    answer = calc(storey);
+
+    return answer;
+}
