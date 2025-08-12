37f05d0acfc9bb3c33b75cdd9c1340303c278cea
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-08-13 01:33:25 +0900

    [PGS] 동영상 재생기/Lv.1/1시간 30분
    https://school.programmers.co.kr/learn/courses/30/lessons/340213?language=cpp
    
    중복되는 구문을 함수로 만들기 연습 필요

---
+++ alsrudwls/Programmers Lv.1 동영상 재생기.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+int ToSecond(const string& time)
+{
+    int m, s;
+    char colon;
+    istringstream iss(time);
+    iss >> m >> colon >> s;
+    return m * 60 + s;
+}
+
+string ToStringTime(int time)
+{
+    int m = time / 60;
+    int s = time % 60;
+    ostringstream oss;
+    oss << setw(2) << setfill('0') << m << ":" << setw(2) << setfill('0') << s;
+    return oss.str();
+}
+
+void skipOpening(int& pos, int op_start, int op_end)
+{
+    if (op_start <= pos && pos <= op_end)
+    {
+        pos = op_end;
+    }
+}
+
+void prev(int& pos, int op_start, int op_end)
+{
+    skipOpening(pos, op_start, op_end);
+
+    pos = max(0, pos - 10);
+
+    skipOpening(pos, op_start, op_end);
+}
+
+void next(int& pos, int op_start, int op_end, int video_len)
+{
+    skipOpening(pos, op_start, op_end);
+
+    pos += 10;
+    if (pos > video_len)
+    {
+        pos = video_len;
+    }
+
+    skipOpening(pos, op_start, op_end);
+}
+
+string solution(string video_len, string pos, string op_start, string op_end, vector<string> commands)
+{
+    int video_len_sec = ToSecond(video_len);
+    int pos_sec = ToSecond(pos);
+    int op_start_sec = ToSecond(op_start);
+    int op_end_sec = ToSecond(op_end);
+
+    for (const string& cmd : commands)
+    {
+        if (cmd == "prev")
+        {
+            prev(pos_sec, op_start_sec, op_end_sec);
+        }
+
+        else if (cmd == "next")
+        {
+            next(pos_sec, op_start_sec, op_end_sec, video_len_sec);
+        }
+    }
+
+    return ToStringTime(pos_sec);
+}
