ebb73379a3bd8a0d3fb1bd3c26b56894b8e0e62a
minJeongHwan9651 <166204973+minJeongHwan9651@users.noreply.github.com>
2025-08-14 16:45:35 +0900

    [BOJ] / ? / 1로 만들기 / 1시간

---
+++ JhMin/BOJ_1463.py
@@ -0,0 +1,33 @@
+import sys
+
+limit = sys.setrecursionlimit(100000000)
+
+dp = [100000] * 1000006
+
+def dfs(x):
+    if x == 1:
+        return
+    if x % 3 == 0:
+        nx = x // 3
+        if dp[nx] > dp[x] + 1:
+            dp[nx] = dp[x] + 1
+            dfs(nx)
+
+    if x % 2 == 0:
+        nx = x // 2
+        if dp[nx] > dp[x] + 1:
+            dp[nx] = dp[x] + 1
+            dfs(nx)
+    
+    nx = x - 1
+    if dp[nx] > dp[x] + 1:
+            dp[nx] = dp[x] + 1
+            dfs(nx)
+
+n = int(input())
+
+dp[n] = 0
+
+dfs(n)
+
+print(dp[1])
