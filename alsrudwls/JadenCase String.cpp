aff2ca827111f933c146952a669d954c847be732
Minkj <91407973+Ruanur@users.noreply.github.com>
2026-03-29 06:53:55 +0900

    JadenCase 문자열 만들기/Lv.2/40분
    https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=cpp

---
+++ alsrudwls/JadenCase String.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <sstream>
+#include <cctype>
+
+using namespace std;
+
+string solution(string s) {
+    bool bStart_JadenCase = true;
+
+    for (char& c : s)
+    {
+        if (c == ' ')
+        {
+            bStart_JadenCase = true;
+        }
+        else if (bStart_JadenCase)
+        {
+            c = toupper(c);
+            bStart_JadenCase = false;
+        }
+        else
+        {
+            c = tolower(c);
+        }
+
+    }
+
+    string answer = s;
+
+    return answer;
+}
