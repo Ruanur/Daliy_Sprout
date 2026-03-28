1ec369c246185204d452e8efe5ebd3bdee93bafe
Minkj <91407973+Ruanur@users.noreply.github.com>
2026-03-29 06:03:12 +0900

    [PGS] 최댓값과 최솟값/Lv.2/30분
    https://school.programmers.co.kr/learn/courses/30/lessons/12939#

---
+++ alsrudwls/Programmers Lv.2 최댓값과 최솟값.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string text_s;
+    vector<string> string_arr_int;
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> text_s;
+        string_arr_int.push_back(text_s);
+    }
+
+    sort(string_arr_int.begin(), string_arr_int.end(), [](const string& a, const string& b)
+        {
+            return stoi(a) < stoi(b);
+        });
+
+    int pointThisarr = 0;
+    int vector_size = string_arr_int.size();
+    for (int i = 0; i < vector_size; i++)
+    {
+        pointThisarr = stoi(string_arr_int.back());
+        string_arr_int.pop_back();
+        std::cout << pointThisarr;
+    }
+
+
+    
+}
+
+
