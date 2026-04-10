e07bfb97d011647e99541515c4d0e3e1e6349ce3
Minkj <91407973+Ruanur@users.noreply.github.com>
2026-04-10 15:28:43 +0900

    [PGS] 최솟값 만들기/Lv.2/20분
    https://school.programmers.co.kr/learn/courses/30/lessons/12941

---
+++ alsrudwls/Programmers Lv.2 최솟값 만들기.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int Adding_Result_Multiple(vector<int>& A, vector<int>& B)
+{
+    int Result = 0;
+    for (int i = 0; i < A.size(); i++)
+    {
+        Result += A[i] * B[i];
+    }
+
+    return Result;
+}
+
+int solution(vector<int> A, vector<int> B)
+{
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+
+    int answer = Adding_Result_Multiple(A, B);
+
+    return answer;
+}
