aac24a056462f30e29d78312909dffe95cf46174
Minkj <91407973+Ruanur@users.noreply.github.com>
2026-05-15 09:23:54 +0900

    [PGS] 점프와 순간이동/Lv.2/20분
    https://school.programmers.co.kr/learn/courses/30/lessons/12980

---
+++ alsrudwls/Programmers Lv.2 점프와 순간이동.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Calculation(int range)
+{
+    int battery_count = 0; 
+    int temp = range;
+    while (temp)
+    {
+        if (temp % 2)
+        {
+            battery_count++;
+        }
+        temp = temp / 2;
+    }
+    return battery_count;
+}
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+
+    int result = Calculation(n);
+
+    std::cout << result;
+}
