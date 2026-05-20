b407749b6d0c3a3abc10b8be6e1cd70f39355bda
Minkj <91407973+Ruanur@users.noreply.github.com>
2026-05-20 09:00:37 +0900

    [PGS] 멀리뛰기/Lv.2/20
    https://school.programmers.co.kr/learn/courses/30/lessons/12914

---
+++ alsrudwls/Programmers Lv.2 멀리뛰기.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int dp[2001];
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    if (n == 1)
+    {
+        cout << "1";
+        return 0;
+    }
+    else if (n == 2)
+    {
+        cout << "2";
+        return 0;
+    }
+    else
+    {
+        for (int i = 3; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+    }
+
+    long long answer = dp[n] % 1234567;
+    std::cout << answer;
+}
