5d44b2f069ae2868c1f20dddb9f3fabcf7e0777e
Minkj <91407973+Ruanur@users.noreply.github.com>
2026-05-26 16:09:25 +0900

    [PGS] 귤 고르기/Lv.2/40분
    https://school.programmers.co.kr/learn/courses/30/lessons/138476

---
+++ alsrudwls/Programmers Lv.2 귤 고르기.cpp
@@ -0,0 +1,41 @@
+﻿#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, vector<int> tangerine) {
+    unordered_map<int, int> count;
+
+    for (int size : tangerine)
+    {
+        count[size]++;
+    }
+
+    vector<int> counts;
+
+    for (auto pair : count)
+    {
+        counts.push_back(pair.second);
+    }
+
+    // 큰 수 부터 정렬
+    sort(counts.begin(), counts.end(), greater<int>());
+
+    int sum = 0;
+    int answer = 0;
+
+    for (int c : counts)
+    {
+        sum += c;
+        answer++;
+
+        if (sum >= k)
+        {
+            break;
+        }
+    }
+
+    return answer;
+}
