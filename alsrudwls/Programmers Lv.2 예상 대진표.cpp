4b0b3a57da3cd40e532fba89ada618fe9a9234e8
Minkj <91407973+Ruanur@users.noreply.github.com>
2026-06-04 03:43:40 +0900

    [PGS]/Lv.2/예상 대진표/30분
    https://school.programmers.co.kr/learn/courses/30/lessons/12985

---
+++ alsrudwls/Programmers Lv.2 예상 대진표.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = log2(n);
+
+    while (answer > 0)
+    {
+        int half_match_team = n / 2;
+
+        bool aLeft = a <= half_match_team;
+        bool bLeft = b <= half_match_team;
+
+        if (aLeft != bLeft)
+        {
+            return answer;
+        }
+
+        if (!aLeft && !bLeft)
+        {
+            a -= half_match_team;
+            b -= half_match_team;
+        }
+
+        n /= 2;
+        answer--;
+    }
+
+    return answer;
+}
